5db3730db611df36cff4a66f7558290d056bfe98
BAKYA-0203 <bhackiyasivasuppiramaniyan@gmail.com>
2023-10-16 12:08:05 +0530

    first commit

---
+++ maximum.py
@@ -0,0 +1,23 @@
+### Max
+
+def max(a,b,c):
+    if(a>b)&(a>c):
+        print("a and b is a large number")
+    elif(b>c):
+        print("b and c is a large number")
+    else:
+        print("a and c is a large number")
+max(100,50,25)
+
+
+
+### Min
+
+def lowest(a,b,c):
+    if(a<b)&(a<c):
+        print("a and b is a lowest number")
+    elif(b<c):
+        print("b and c is a lowest number")
+    else:
+        print("a and c is a lowest number")
+lowest(90,99,100)
