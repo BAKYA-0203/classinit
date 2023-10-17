daad7313deb7af1751be08a198373e24db58b0f6
BAKYA-0203 <146165648+BAKYA-0203@users.noreply.github.com>
2023-10-17 11:26:15 +0530

    Add files via upload

---
+++ elseif.py
@@ -0,0 +1,9 @@
+a=int(input("Enter the number: "))
+b=int(input("Enter the number: "))
+c=int(input("Enter the number: "))
+if(a>b & a>c):
+    print("a is big")
+elif(b>c):
+    print("b is big")
+else:
+    print("c is big")
